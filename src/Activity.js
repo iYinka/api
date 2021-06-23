3d91c59f30f88a04f98c540d548e1f615ecf6ea0
Olayinka Victor Owolabi <olayinkav28@gmail.com>
2021-06-23 15:06:21 +0100

    first commit for API calls

---
+++ src/Activity.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Activity() {
+  return (
+    <div>
+      <Link to='/'>
+        <button>Go TO Home</button>  
+      </Link>
+    </div>
+  )
+}
+
+export default Activity
---
+++ src/Logo.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Input } from 'antd';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import './App.css'
+import {Link, useHistory} from 'react-router-dom'
+
+
+function Logo() {
+  // const history = useHistory();
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+
+  // const SignIn = e => {
+  //   e.preventDefault();
+  //   history.push('/activity')
+  // }
+
+  // const handleChange = (name) => ({target}) => {
+  //   setValues({
+  //     ...values,
+  //     [name]: target.value
+  //     })
+  // }
+  return (
+    <div className='input'>
+      <Input placeholder='Enter email' value={values.email}
+        // onChange={handleChange('email')} 
+        
+        />
+      <Input.Password
+        value={values.password}
+      placeholder="Enter password"
+        iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+        // onChange={handleChange('password')}
+      />
+      <Link to='/activity'>
+              <button >Log in...</button>
+
+      </Link>
+    </div>
+  )
+}
+
+export default Logo
